4231ea314af11a1f309f44bf3627e1975816b840
Frederick Ankomah <ankomahfrederickyahoo@gmail.com>
2024-10-07 22:42:44 +0000

    Add validation logic

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ const form = document.getElementById('registration-form');
 
 const feedbackDiv = document.getElementById('form-feedback');
 
-const username = documnent.getElementById('');
+const username = document.getElementById('username').value.trim();
 
-const email = document.getElementById('');
+const email = document.getElementById('email').value.trm();
 
-const password = document.getElementById('');
+const password = document.getElementById('password').value.trim();
+
+let isValid = true;
+
+let messages = [];
 
 
 form.addEventListener('submit', onSubmit);
@@ -16,13 +20,10 @@ form.addEventListener('submit', onSubmit);
 
 function onSubmit(event){
     event.preventDefault();
+
+    if(username.length < 3){
+        isValid = false;
+        messages.push("Invalid User Name");
+    }
 }
-
-
-
-
-
-
-
-
-})
+});
