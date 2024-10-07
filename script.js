f0ab9ac59abdd44051dee7e854617f95356f7948
Frederick Ankomah <ankomahfrederickyahoo@gmail.com>
2024-10-07 17:30:43 +0000

    Add index.html, style.css and script.js to the project

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+const form = document.getElementById('registration-form');
+
+const feedbackDiv = document.getElementById('form-feedback');
+
+const username = documnent.getElementById('');
+
+const email = document.getElementById('');
+
+const password = document.getElementById('');
+
+
+form.addEventListener('submit', onSubmit);
+
+
+function onSubmit(event){
+    event.preventDefault();
+}
+
+
+
+
+
+
+
+
+})
