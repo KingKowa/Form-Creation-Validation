a8062c6be04778b19ee51690bd6bf30f61e97153
Frederick Ankomah <ankomahfrederickyahoo@gmail.com>
2024-10-08 22:04:03 +0000

    Add fetch.html, fetch.css, and fetch.js to project

---
+++ fetch-data.js
@@ -0,0 +1,10 @@
+async function fetchUserData(params) {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+    const dataContainer = document.getElementById('api-data');
+
+    try {
+        const response = await fetch(apiUrl);
+    }catch(error){
+
+    }
+}
